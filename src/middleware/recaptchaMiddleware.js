941ca6e5eada103ee6c12f0cddf6fe0aa9ceb0e2
Jean <165350183+jeanPersil@users.noreply.github.com>
2025-12-09 14:53:05 -0300

    ajuste no middleware do recapcha

--- src/middleware/recaptchaMiddleware.js
+++ src/middleware/recaptchaMiddleware.js
@@ -1,40 +1,38 @@
 import axios from "axios";
 
 export const validarRecaptcha = async (req, res, next) => {
-  const recaptchaToken = req.body.recaptchaToken;
+  const token = req.body.recaptchaToken;
 
-  if (!recaptchaToken) {
-    return res.status(400).json({ error: "Token do reCAPTCHA não fornecido." });
+  if (!token) {
+    return res.status(400).json({
+      error: "Token do reCAPTCHA não foi enviado.",
+    });
   }
 
-  const secretKey = process.env.RECAPTCHA_SECRET;
-  const googleURL = `https://www.google.com/recaptcha/api/siteverify`;
-
   try {
-    const response = await axios.post(googleURL, null, {
-      params: {
-        secret: secretKey,
-        response: recaptchaToken,
-      },
-    });
+    const { data } = await axios.post(
+      "https://www.google.com/recaptcha/api/siteverify",
+      null,
+      {
+        params: {
+          secret: process.env.RECAPTCHA_SECRET,
+          response: token,
+        },
+      }
+    );
 
-    const { success, "error-codes": errorCodes } = response.data;
-
-    if (!success) {
-      console.error(`Erro reCAPTCHA: ${JSON.stringify(errorCodes)}`);
+    if (!data.success) {
       return res.status(400).json({
-        error: "Falha na validação do robô (reCAPTCHA).",
-        details:
-          `Erro no reCAPTCHA: ${JSON.stringify(errorCodes)}` ||
-          "Token inválido ou expirado.",
+        error: "Falha ao validar reCAPTCHA.",
+        details: data["error-codes"],
       });
     }
 
     next();
-  } catch (error) {
-    console.error("Erro ao conectar com Google:", error);
-    return res
-      .status(500)
-      .json({ error: "Erro interno ao validar reCAPTCHA." });
+  } catch (err) {
+    console.error("Erro ao validar reCAPTCHA:", err);
+    return res.status(500).json({
+      error: "Erro ao conectar com Google.",
+    });
   }
 };
